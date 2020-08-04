7771f2bf74a2b6927c1a6a9a640598dd1e0b1bc0
annesophiegendron <annesophie.gendron@gmail.com>
2020-08-04 12:36:39 +0200

    fixing pictures size

--- my-app/src/assets/serieOne/image-data-serieOne.js
+++ my-app/src/assets/serieOne/image-data-serieOne.js
@@ -1,4 +1,5 @@
 export const images = [
   "https://res.cloudinary.com/dc2tplzzd/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1596449713/photography%20website/57CDE44F-C8A3-4E60-9C3F-E40BED05C55A_1_105_c_rmytjd.jpg",
   "https://res.cloudinary.com/dc2tplzzd/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1596451765/photography%20website/photo1_nhbzg8.jpg",
+  "https://res.cloudinary.com/dc2tplzzd/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1596458844/photography%20website/11EBA3B9-B41C-4B9B-A222-2DCCACACED8F_m4on2x.jpg"
 ]
